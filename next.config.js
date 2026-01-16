6af88e821ae8be0fb8b4971c79ddaf4c74cacd73
Kok Shew Juan <kokshewjuan7@gmail.com>
2026-01-16 00:41:06 +0000

    Run actions/configure-pages@v5 Injecting property=output and value=expor

---
+++ next.config.js
@@ -0,0 +1,26 @@
+const nextBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+};
+
+module.exports = nextBundleAnalyzer(nextConfig);
